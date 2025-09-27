170b14aa79736e7114d0c0591ae41f039f68f4f6
king6870 <kingjacobisthegoat@gmail.com>
2025-09-27 12:51:13 -0700

    frontendchange

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export const meta: MetaFunction = () => [
   { title: "OpenHands" },
-  { name: "description", content: "Let's Start Building with Azure!" },
+  { name: "description", content: "Let's Start Building with Azure Models!" },
 ];
 
 export default function App() {
